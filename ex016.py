b3e61a8edc0235fa6c6d1af84529ce918adcc659
Luiz <jc381311@gmail.com>
2025-09-03 10:09:50 -0300

    novos exercicios

---
+++ ex016.py
@@ -0,0 +1,4 @@
+salary = float(input('Salário atual dê:'))
+increase = salary * 0.15
+finalIncrease = salary + increase
+print(f'O salário atual dê {salary:.2f}, subiu 15% e foi para o valor dê: {finalIncrease:.2f}')
